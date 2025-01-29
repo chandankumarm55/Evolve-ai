49dfd2dae2202389c178460a9263583013e3235b
Chandan Kumar <149804185+chandankumarm55@users.noreply.github.com>
2025-01-29 22:31:48 +0530

    added the code

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        port: 3000, // Change port if needed
-        open: true, // Automatically open browser
-    },
-    resolve: {
-        alias: {
-            "@": "/src", // Allows importing like "@/components/Component"
-        },
-    },
-    build: {
-        outDir: "dist",
-        sourcemap: true, // Helps with debugging
-    },
-});
+})
